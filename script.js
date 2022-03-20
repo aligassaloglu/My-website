65665e5886bc09f32d19cfab2f6c2eb44d490ffd
ali gassaloglu <aligassaloglu@hotmail.co.uk>
2022-03-20 21:44:34 +0300

    update
    -bug fix

--- script.js
+++ script.js
@@ -42,18 +42,18 @@ setInterval(countdown, 1000)
 
 
 
-window.onload = function () {
-    // Array of Images
-     var backgroundImg=[
-        "adrian-swancar-pIr-PH5IXF8-unsplash.jpg",
-        "greg-rakozy-oMpAz-DN-9I-unsplash.jpg",
-        "ibrahim-shabil-VE7VS0C9K1k-unsplash.jpg"
-        ]
+// window.onload = function () {
+//     // Array of Images
+//      var backgroundImg=[
+//         "adrian-swancar-pIr-PH5IXF8-unsplash.jpg",
+//         "greg-rakozy-oMpAz-DN-9I-unsplash.jpg",
+//         "ibrahim-shabil-VE7VS0C9K1k-unsplash.jpg"
+//         ]
 
-       setInterval(changeImage, 5000);
-      function changeImage() {   
-       var i = Math.floor((Math.random() * 3));
+//        setInterval(changeImage, 5000);
+//       function changeImage() {   
+//        var i = Math.floor((Math.random() * 3));
 
-       document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";
-     }
-   }
+//        document.body.style.backgroundImage = "url('"+backgroundImg[i]+"')";
+//      }
+//    }
